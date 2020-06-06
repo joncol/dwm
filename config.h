03a098fca957f7f0fdfcded52f59eee3fa46ea43
Jonas Collberg <jonas.collberg@gmail.com>
2020-06-06 17:37:06 +0200

    Add commands for changing volume

--- config.h
+++ config.h
@@ -70,9 +70,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_20000_leagues_under_the_sea, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
-static const char *lockcmd[]  = { "slock", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_20000_leagues_under_the_sea, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "st", "-e", "tmux", NULL };
+static const char *lockcmd[]    = { "slock", NULL };
+static const char *voldowncmd[] = { ".local/bin/lower_volume.sh", NULL };
+static const char *volupcmd[]   = { ".local/bin/raise_volume.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -131,6 +133,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_Delete, quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_x,      spawn,          {.v = lockcmd } },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = voldowncmd } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = volupcmd } },
 };
 
 /* button definitions */
